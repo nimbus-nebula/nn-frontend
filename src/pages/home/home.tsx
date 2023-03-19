ffd169a1e84abb95e6280f4e2e3ab94e3f35640b
triplekingg <tripleking123@gmail.com>
2023-03-19 14:29:28 +0700

    fix: Use CSS to make background and logo

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../App.css';
 
 const logo: string = `${process.env.PUBLIC_URL}/nn-logo.svg`;
+const divider: string = `${process.env.PUBLIC_URL}/section-div.svg`;
 
 function OnLogin(){
 
@@ -10,18 +11,31 @@ function Home() {
     return (
         <div className="app">
             <header className="app-header">
-                <img src={logo} className="app-logo" alt="logo"/>
-                <p>
-                    "Create Your Cluster of Files"
-                </p>
-                <form>
-                    <h3> Email </h3>
+                <div className='logo-section'>
+                    <div className='logo-n'>
+                        N
+                        <div className='logo-remaining'>inbus ebula</div>
+                    </div>
+                    <div className='app-slogan'>
+                        "Create Your Cluster of Files"
+                    </div>
+                </div>
+                
+                {/* <img src={logo} className="app-logo" alt="logo"/> */}
+                {/* <img src={divider} className="section-divider" alt="logo"/> */}
+                
+                {/* <hr className='solid'/> */}
+                <form className='login-form'>
+                    <h3 className='input-label'> Email </h3>
                     <input type="text" id="fname" name="fname" placeholder={"Email"}/> <br/>
                     <h3> Password </h3>
                     <input type="password" id="fname" name="fname" placeholder={"Password"}/> <br/>
                     <input type={"submit"} value={"Log In"} onSubmit={OnLogin}/>
                 </form>
             </header>
+            <body>
+            
+            </body>
         </div>
     );
 }
