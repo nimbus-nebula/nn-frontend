181e8e366adda3a76826ee8a568986f8234420c9
khingxc <khingkhwanc@gmail.com>
2023-03-23 00:30:29 +0700

    fix: customizing forms in login/signup pages

--- src/components/home/home-components.tsx
+++ src/components/home/home-components.tsx
@@ -1,111 +1,53 @@
 import React from "react";
-import { useState } from "react";
-import { Button, Form, Input, Select } from "antd";
-
-const { Option } = Select;
-
-interface DataNodeType {
-  value: string;
-  label: string;
-  children?: DataNodeType[];
-}
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 8 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 16 },
-  },
-};
-
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0,
-    },
-    sm: {
-      span: 16,
-      offset: 8,
-    },
-  },
-};
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Space } from "antd";
 
 export const LogInForm: React.FC = () => {
-  const [form] = Form.useForm();
-
   const onFinish = (values: any) => {
     console.log("Received values of form: ", values);
   };
 
-  const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([]);
-
-  const onWebsiteChange = (value: string) => {
-    if (!value) {
-      setAutoCompleteResult([]);
-    } else {
-      setAutoCompleteResult(
-        [".com", ".org", ".net"].map((domain) => `${value}${domain}`)
-      );
-    }
-  };
-
-  const websiteOptions = autoCompleteResult.map((website) => ({
-    label: website,
-    value: website,
-  }));
-
   return (
     <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
+      name="normal_login"
+      className="form-login"
+      initialValues={{ remember: true }}
       onFinish={onFinish}
-      initialValues={{
-        residence: ["zhejiang", "hangzhou", "xihu"],
-        prefix: "86",
-      }}
-      style={{ maxWidth: 600 }}
-      scrollToFirstError
+      size="large"
     >
       <Form.Item
         name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "This field is required!",
-          },
-        ]}
+        rules={[{ required: true, message: "Please input your Email!" }]}
       >
-        <Input />
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Email"
+        />
       </Form.Item>
-
       <Form.Item
         name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: "This field is required!",
-          },
-        ]}
-        hasFeedback
+        rules={[{ required: true, message: "Please input your Password!" }]}
       >
-        <Input.Password />
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
       </Form.Item>
-
-      <Form.Item {...tailFormItemLayout}>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
+      <Form.Item>
+        <Space direction="vertical" size="middle">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            shape="round"
+          >
+            Log in
+          </Button>
+          <a href="/signup" className="link-to-register-page">
+            Register Now!
+          </a>
+        </Space>
       </Form.Item>
     </Form>
   );
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,29 +1,14 @@
 import React from "react";
-import "../../App.css";
+// import "../../App.css";
 import {
   Logo,
   Slogan,
-  DataField,
-  Link,
-  PasswordField,
 } from "../global-components";
 import "./home-components";
 import { LogInForm } from "./home-components";
+import "./home.css";
 
 const divider: string = `${process.env.PUBLIC_URL}/section-div.svg`;
-
-function SubmitButton() {
-  return (
-    <input
-      className="submit-button"
-      type={"submit"}
-      value={"Log In"}
-      onSubmit={OnLogin}
-    />
-  );
-}
-
-function OnLogin() {}
 
 function Home() {
   return (
@@ -39,7 +24,7 @@ function Home() {
         </div>
         <form className="login-form">
           <LogInForm></LogInForm>
-          <Link Href="/signup" Text="Create An Account"></Link>
+          {/*<Link Href="/signup" Text="Create An Account"></Link>*/}
         </form>
       </body>
     </div>
